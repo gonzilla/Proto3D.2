46fdad0c7d2296285c5f1463e9c61b4d43e4421c
Lacassagne <godzilla77000@hotmail.fr>
2021-02-18 18:28:20 +0100

    Premier version
    Testo

---
+++ Assets/Script/CarControlleur.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+    public enum TypeDeroue
+    {
+        Avant,
+        Arriere
+
+    };
+    public TypeDeroue roue;
+}
+
+public class CarControlleur : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public bool RoueJoystick = false;
+    
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        if (RoueJoystick)
+        {
+            MoveAvecRoue();
+        }
+        else if (!RoueJoystick)
+        {
+            MoveSansRoue();
+        }
+       
+    }
+
+    void MoveAvecRoue() 
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Acceleration");
+        //float steering = maxSteeringAngle * Input.GetAxis("HorizontalManette");
+        float X = Input.GetAxis("HorizontalManette");
+        float Z = Input.GetAxis("VerticalManette");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.roue == AxleInfo.TypeDeroue.Arriere)
+            {
+                X = Input.GetAxis("HorizontalManetteDroit");
+                Z = Input.GetAxis("VerticalManetteDroit");
+                //steering = maxSteeringAngle * Input.GetAxis("HorizontalManetteDroit");
+            }
+            else if (axleInfo.roue == AxleInfo.TypeDeroue.Avant)
+            {
+                X = Input.GetAxis("HorizontalManette");
+                Z = Input.GetAxis("VerticalManette");
+                //Quaternion.Euler(0, Mathf.Atan2(X, Z) * Mathf.Rad2Deg, 0);
+                //steering = maxSteeringAngle * Input.GetAxis("HorizontalManette");
+            }
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = Mathf.Atan2(X, Z) * Mathf.Rad2Deg;
+                axleInfo.rightWheel.steerAngle = Mathf.Atan2(X, Z) * Mathf.Rad2Deg;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            //ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            //ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+    }
+
+    void MoveSansRoue()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Acceleration");
+        float steering = maxSteeringAngle * Input.GetAxis("HorizontalManette");
+        
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.roue == AxleInfo.TypeDeroue.Arriere)
+            {
+               steering = maxSteeringAngle * Input.GetAxis("HorizontalManetteDroit");
+            }
+            else if (axleInfo.roue == AxleInfo.TypeDeroue.Avant)
+            {
+               
+                
+                steering = maxSteeringAngle * Input.GetAxis("HorizontalManette");
+            }
+            if (axleInfo.steering)
+            {
+                
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+    }
+
+}
---
+++ Assets/Script/MotoControlleur.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfoMoto
+{
+    public WheelCollider Wheely;
+    public bool motor;
+    public bool steering;
+    public enum TypeDeroue
+    {
+        Avant,
+        Arriere
+
+    };
+    public TypeDeroue roue;
+}
+public class MotoControlleur : MonoBehaviour
+{
+    public List<AxleInfoMoto> axleInfosMoto;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public bool RoueJoystick = true;
+    public bool Inclinaison = true;
+    public float AngleInlinaisonMax;
+    public Transform siInclinaison;
+    public float SpeedSteering;
+
+    float ActualStreeringFront;
+    float ActualStreeringBack;
+
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        if (RoueJoystick)
+        {
+            MoveAvecRoue();
+        }
+        else if (!RoueJoystick)
+        {
+            MoveSansRoue();
+        }
+        
+    }
+
+
+    void MoveAvecRoue() 
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Acceleration");
+        float X = Input.GetAxis("HorizontalManette");
+        float Z = Input.GetAxis("VerticalManette");
+        bool roueavant = false;
+
+        foreach (AxleInfoMoto axleInfoM in axleInfosMoto)
+        {
+            if (axleInfoM.roue == AxleInfoMoto.TypeDeroue.Arriere)
+            {
+                X = Input.GetAxis("HorizontalManetteDroit");
+                Z = Input.GetAxis("VerticalManetteDroit");
+                roueavant = false;
+            }
+            else if (axleInfoM.roue == AxleInfoMoto.TypeDeroue.Avant)
+            {
+                X = Input.GetAxis("HorizontalManette");
+                Z = Input.GetAxis("VerticalManette");
+                roueavant = true;
+                
+            }
+            if (axleInfoM.steering)
+            {
+
+                calculDusteeringDeuxRoue(Mathf.Atan2(X, Z) * Mathf.Rad2Deg, roueavant);
+                if (roueavant)
+                {
+                    axleInfoM.Wheely.steerAngle = ActualStreeringFront;
+                }
+                else if (!roueavant)
+                {
+                    axleInfoM.Wheely.steerAngle = ActualStreeringBack;
+                }
+                
+                
+            }
+            if (axleInfoM.motor)
+            {
+                axleInfoM.Wheely.motorTorque = motor;
+                
+            }
+            
+        }
+    }
+    void MoveSansRoue()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Acceleration");
+        float steering = maxSteeringAngle * Input.GetAxis("HorizontalManette");
+        float inclinaison = AngleInlinaisonMax * Input.GetAxis("HorizontalManetteDroit");
+        string axepoursteering = "HorizontalManette";
+        foreach (AxleInfoMoto axleInfoM in axleInfosMoto)
+        {
+            if (axleInfoM.roue == AxleInfoMoto.TypeDeroue.Arriere)
+            {
+                axepoursteering = "HorizontalManetteDroit";
+
+                //steering = maxSteeringAngle * Input.GetAxis("HorizontalManetteDroit");
+            }
+            else if (axleInfoM.roue == AxleInfoMoto.TypeDeroue.Avant)
+            {
+                axepoursteering = "HorizontalManette";
+                //steering = maxSteeringAngle * Input.GetAxis("HorizontalManette");
+            }
+            if (axleInfoM.steering)
+            {
+
+                calculDusteering(axepoursteering);
+                axleInfoM.Wheely.steerAngle = ActualStreeringFront;
+                //axleInfoM.Wheely.steerAngle = Mathf.Lerp(-maxSteeringAngle, maxSteeringAngle, steering * Time.deltaTime * SpeedSteering); ;
+            }
+            if (axleInfoM.motor)
+            {
+                axleInfoM.Wheely.motorTorque = motor;
+               
+            }
+            ApplyLocalPositionToVisuals(axleInfoM.Wheely);
+           
+        }
+        if (Inclinaison)
+        {
+            siInclinaison.rotation = Quaternion.Euler(0, 0, -inclinaison);
+        }
+        
+    }
+
+    void calculDusteering(string axis) //, out float steeringCal
+    {
+    float steering = maxSteeringAngle * Input.GetAxis(axis);
+        if (steering!=0)
+        {
+            if (ActualStreeringFront<=maxSteeringAngle && ActualStreeringFront>=-maxSteeringAngle)
+            {
+                ActualStreeringFront += steering * Time.deltaTime * SpeedSteering;
+                if (ActualStreeringFront>maxSteeringAngle)
+                {
+                    ActualStreeringFront = maxSteeringAngle;
+                }
+                if (ActualStreeringFront<-maxSteeringAngle)
+                {
+                    ActualStreeringFront = -maxSteeringAngle;
+                }
+            }
+        }
+        else if (steering==0 && ActualStreeringFront !=0)
+        {
+            ActualStreeringFront = Mathf.Lerp(ActualStreeringFront, 0, Time.deltaTime * SpeedSteering);
+        }
+
+        //float A = Mathf.Lerp(-maxSteeringAngle, maxSteeringAngle, steering * Time.deltaTime * SpeedSteering);
+    }
+    void calculDusteeringDeuxRoue(float ValueViser,bool RoueAvant) 
+    {
+        float steering = 0;
+        if (RoueAvant)
+        {
+            steering = ValueViser;
+            ActualStreeringFront = Mathf.Lerp(ActualStreeringFront, steering, SpeedSteering * Time.deltaTime); 
+        }
+        if (!RoueAvant)
+        {
+            steering = ValueViser;
+            ActualStreeringBack = Mathf.Lerp(ActualStreeringBack, steering, SpeedSteering * Time.deltaTime);
+
+        }
+        
+        
+        
+        
+        //ActualStreeringFront = Mathf.Lerp(ActualStreeringFront,ValueViser,SpeedSteering*Time.deltaTime);
+        //ActualStreeringBack = Mathf.Lerp(ActualStreeringBack, ValueViser, SpeedSteering * Time.deltaTime);
+    }
+}
