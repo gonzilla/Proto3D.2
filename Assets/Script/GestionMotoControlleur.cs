4112a6d63073f790447352da251f3d5c85850a33
Lacassagne <godzilla77000@hotmail.fr>
2021-03-21 16:38:53 +0100

    Nettoyage du projet
    - Retirer amplify Shader
    - Retirer cinemachine
    - Retirer script inutile
    - Trier dossier
    - Préparer dossier pour future asset du projet
    - suppression d'élément dont je ne savais pas l'utilité

---
+++ Assets/Script/GestionMotoControlleur.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionMotoControlleur : PersonnalMethod
+{
+    //Public variable
+    public float VitesseMoto;
+    public float AccélérationMoto,vitesseMax;
+    public AnimationCurve AccelerationSelonVitesseMax;
+    public float PuissanceFrainage;
+    public AnimationCurve FreinageSelonVitesse;
+    public float angleMaxInAir;
+    public AnimationCurve Ralentissement;
+    public float ForceRalentissement;
+
+
+    public float VitesseRotation;
+
+    public float DecellerationVitesseElleve;
+
+    public Transform detecteur;
+    public float GroundRayLength;
+    public LayerMask whatIsGround;
+    //Local variable
+    Quaternion RotationHorsSol;
+    Vector3 DirectionMoto;
+    bool grounded;
+    Rigidbody Rb;
+    GestionGeneral GG;
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        GetGestion(out GG, this.gameObject);
+    }
+    public void SetByNormal() 
+    {
+
+        grounded = false;
+        RaycastHit hit;
+       
+       
+        if (Physics.Raycast(detecteur.position, -transform.up, out hit, GroundRayLength, whatIsGround))
+        {
+            //print("touche terrain");
+            grounded = true;
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            Debug.DrawRay(detecteur.position, -transform.up * GroundRayLength, Color.magenta);
+        }
+
+        
+
+
+    }
+    
+    public void avance(float Direction) 
+    {
+        
+            
+                
+                
+            float DirectionMoteur = 0;
+            float DirectionVoulue = 0;
+            float pourcentage = Mathf.Abs( VitesseMoto) / vitesseMax;
+            if (VitesseMoto != 0)
+            {
+                DirectionMoteur = VitesseMoto / Mathf.Abs(VitesseMoto);
+            }
+            if (Direction != 0)
+            {
+                DirectionVoulue = Direction / Mathf.Abs(Direction);
+            }
+
+            if (DirectionMoteur != DirectionVoulue && DirectionMoteur != 0)
+            {
+                Freine(DirectionVoulue, Direction, pourcentage);
+            }
+            else if (DirectionMoteur == DirectionVoulue || DirectionMoteur == 0)
+            {
+                if (grounded){
+                if (VitesseMoto<vitesseMax)
+                {
+                    VitesseMoto += AccélérationMoto * Direction * Time.deltaTime * AccelerationSelonVitesseMax.Evaluate(pourcentage);
+                }
+                
+                checkVitesseMoto();
+                }
+            }
+        /*if (grounded)
+        {
+            transform.Translate(transform.forward * VitesseMoto, Space.World);
+        }    
+        else if (!grounded) 
+        {
+            transform.Translate(Vector3.zero * VitesseMoto, Space.World);
+        
+        }*/
+
+        transform.Translate(transform.forward * VitesseMoto, Space.World);
+
+
+    }
+    public void tourne(float X) 
+    {
+
+        if (grounded && Mathf.Abs(VitesseMoto)>0.01f )
+        {
+            float Rotation = X * VitesseRotation * Time.deltaTime;
+            transform.rotation *= Quaternion.Euler(0, Rotation, 0);
+
+        }
+        
+       
+        //float Z = Input.GetAxis("VerticalManette");
+        //float angleToGo = X * angleMax;
+        
+        //transform.rotation
+        //Vector3 DirectionRay = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angleToGo), 0, Mathf.Cos(Mathf.Deg2Rad * angleToGo));
+        //Debug.DrawRay(transform.position, DirectionRay * 10, Color.red);
+    }
+
+    public void Freine(float directionFrein, float Input , float pourcentage) 
+    {
+        
+        if ((Input == 0 || !grounded)&& !GG.GB.Surchauffing)
+        {
+            // un faible ralentissement
+            VitesseMoto = Mathf.Lerp(VitesseMoto, 0, ForceRalentissement * Time.deltaTime * Ralentissement.Evaluate(pourcentage));
+            //VitesseMoto += directionFrein*Mathf.Abs(Input)*ForceRalentissement*Input*Time.deltaTime;
+        }
+        else if (grounded && !GG.GB.Surchauffing)
+        {
+
+            VitesseMoto = Mathf.Lerp(VitesseMoto, directionFrein, PuissanceFrainage * Time.deltaTime * FreinageSelonVitesse.Evaluate(pourcentage));
+            // Gros Freinage
+            //VitesseMoto =
+            //VitesseMoto += directionFrein * PuissanceFrainage * Time.deltaTime * FreinageSelonVitesse.Evaluate();
+        }
+
+        checkVitesseMoto();
+    }
+
+    void checkVitesseMoto() 
+    {
+        if (Mathf.Abs(VitesseMoto)-vitesseMax>=0.1)
+        {
+           
+            VitesseMoto -= DecellerationVitesseElleve*Time.deltaTime;
+        }
+        else if (Mathf.Abs(VitesseMoto) - vitesseMax < 0.1)
+        {
+            if (VitesseMoto > vitesseMax)
+            {
+                VitesseMoto = vitesseMax;
+            }
+            else if (VitesseMoto < -vitesseMax)
+            {
+                VitesseMoto = -vitesseMax;
+            }
+            float limite = 0.001f;
+            if (VitesseMoto > -limite && VitesseMoto < limite)
+            {
+                VitesseMoto = 0;
+            }
+        }
+        
+    
+    }
+
+    void FreezeRotation()
+    {
+        
+        
+        if (grounded)
+        {
+            Rb.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        }
+        else
+        {
+            Rb.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+    }//tentative
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ( collision.transform.gameObject.layer == 8)
+        {
+            Rb.velocity = Vector3.zero;
+            
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+        /*if (collision.transform.gameObject.layer == 8)
+        {
+            FreezeRotation();
+
+        }*/
+        
+    }
+    /*private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        float X = Input.GetAxis("HorizontalManette");
+        float angleToGo = X * angleMax;
+        Vector3 coordonnéespehere = transform.position + new Vector3(Mathf.Sin(Mathf.Deg2Rad * angleToGo), 0, Mathf.Cos(Mathf.Deg2Rad * angleToGo)).normalized * AccélérationMoto;
+        Gizmos.DrawWireSphere(coordonnéespehere,0.2f);
+    }*/
+}
