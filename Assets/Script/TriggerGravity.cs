6a895b69c5cb3574126abc1d328d50d25c9a3564
Lacassagne <godzilla77000@hotmail.fr>
2021-05-29 01:26:40 +0200

    Transition smooth

--- Assets/Script/TriggerGravity.cs
+++ Assets/Script/TriggerGravity.cs
@@ -8,7 +8,10 @@ public class TriggerGravity : MonoBehaviour
     public Vector3 NewGravity;
     public float forceGravity;
     public float RotationAngle;
-
+    public float vitesserotation;
+    
+    
+    Quaternion Cible;
     Transform LaMoto;
     void changement() 
     {
@@ -18,9 +21,21 @@ public class TriggerGravity : MonoBehaviour
         }
         if (RotationAngle!=0)
         {
-            LaMoto.transform.rotation *= Quaternion.Euler(0,0, RotationAngle);
+            Cible = LaMoto.transform.rotation * Quaternion.Euler(0, 0, RotationAngle);
+            rotateCam();
         }
     
+    }
+
+    void rotateCam() 
+    {
+        
+        if (LaMoto.transform.rotation!= Cible)
+        {
+            LaMoto.transform.rotation = Quaternion.RotateTowards(LaMoto.transform.rotation, Cible, vitesserotation);
+            Invoke("rotateCam", Time.deltaTime);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
